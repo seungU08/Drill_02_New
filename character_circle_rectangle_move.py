0708cf3915722c48cffe71d3eeae8d8b597b7744
SWnotebook <sungwoo5957@naver.com>
2023-09-21 10:39:15 +0900

    사각형 오른쪽줄

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -29,6 +29,9 @@ def run_rectangle():
 
     for x in range(50,750 +1,10):
         render_frame(x , 90)
+
+    for y in range(90 , 550 +1, 10):
+        render_frame(750 , y)
         
     for x in range(750,50 -1,-10):
         render_frame(x , 550)
