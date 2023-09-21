482df0b550816c3756a1e130c7b168e555b495a8
SWnotebook <sungwoo5957@naver.com>
2023-09-21 10:23:05 +0900

    사각형 아래줄

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -8,10 +8,8 @@ character = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
-
     #일단 그림을 그리자
     cx , cy , r= 800//2 , 600//2 , 200
-    
 
     for deg in range(0,360,1):
         x = r * math.cos(math.radians(deg)) + cx
@@ -22,19 +20,23 @@ def run_circle():
         character.draw_now(x,y)
         delay(0.01)
 
-
-
-    pass
-
 def run_rectangle():
     print('RECTANGLE')
+
+    for x in range(50,750 +1,10):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        delay(0.01)
+
+
     pass
 
 
 
 while True:
-    run_circle()
+    # run_circle()
     run_rectangle()
-    
+    break
 
 close_canvas()
