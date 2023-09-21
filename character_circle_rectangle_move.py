ab3087f8f3150c7b00389e0714e8a24212730e38
SWnotebook <sungwoo5957@naver.com>
2023-09-21 09:58:15 +0900

    무한루프 뼈대

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -5,4 +5,17 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_circle():
+    pass
+
+def run_ractangle():
+    pass
+
+
+
+while True:
+    run_circle()
+    run_ractangle()
+    
+
 close_canvas()
